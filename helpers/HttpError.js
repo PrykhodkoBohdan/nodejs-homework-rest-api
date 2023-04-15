6d2bfa39f55f1fa2030e1e3e29f9791c425b85dc
pickfix <Prikhodko.B.T@gmail.com>
2023-04-16 00:19:40 +0300

    Update HttpError.js

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,6 +1,9 @@
 const errorMessages = {
-    404: "Not found",
-    
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict"  
   }
   
   const HttpError = (status, message = errorMessages[status]) => {
