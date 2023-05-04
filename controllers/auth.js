35c9b69509bdcb26614a0ab7f649ea1f48d33fd7
pickfix <Prikhodko.B.T@gmail.com>
2023-05-04 22:32:09 +0300

    Update auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,112 +1,190 @@
 const jwt = require("jsonwebtoken");
-const {SECRET_KEY} = require("../secret");
+const { SECRET_KEY } = require("../secret");
+const {BASE_URL} = process.env;
 const gravatar = require("gravatar");
 const fs = require("fs/promises");
 const path = require("path");
 const jimp = require("jimp");
+const { v4: uuidv4 } = require('uuid');
 
+const { registerShema, emailShema, User } = require("../models/user");
+const { HttpError, sendEmail } = require("../helpers");
+const bcrypt = require("bcryptjs");
 const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
-const {registerSchema, User} = require("../models/user");
-const { HttpError } = require("../helpers/HttpError");
-const bcrypt = require("bcryptjs");
-
-const register = async(req, res, next) => {
+const register = async (req, res, next) => {
   try {
-    const {error} = registerSchema.validate(req.body);
-    const {email, password} = req.body;
-    if(error) {
-      throw HttpError(400, "Error from Joi or other validation library")
+    const { error } = registerShema.validate(req.body);
+    const { email, password } = req.body;
+
+    if (error) {
+      throw HttpError(400, "Error from Joi or other validation library");
     }
 
-    const user = await User.findOne({email});
-    if(user) {
+    const user = await User.findOne({ email });
+    if (user) {
       throw HttpError(409, "Email in use");
     }
-    const hashPassword = await bcrypt.hash(password, 10);
+
     const avatarURL = gravatar.url(email);
-    const result = await User.create({...req.body, password: hashPassword,avatarURL});
-   
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const verificationToken = uuidv4();
+
+    const result = await User.create({
+      ...req.body,
+      password: hashPassword,
+      avatarURL,
+      verificationToken,
+    });
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Verification email sent</p>`,
+    };
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
       email: result.email,
       subscription: result.subscription,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+const verify = async(req, res, next) => {
+  try {
+    const { verificationToken } = req.params;
+    const user = await User.findOne({verificationToken});
+
+    if(!user) {
+      throw HttpError(404, "User not found");
+    }
+    await User.findByIdAndUpdate(user._id, {verify: true, verificationToken: ""});
+
+    res.json({
+      message: 'Verification successful',
     })
+    
   } catch (error) {
     next(error);
   }
 }
 
-const login = async(req, res, next) => {
+const resendVerify = async (req, res, next) => {
   try {
-    const {error} = registerSchema.validate(req.body);
-    const {email, password} = req.body;
-    if(error) {
-      throw HttpError(400, "Error from Joi or other validation library")
+    const { error } = emailShema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Error from Joi or other validation library");
+    }
+    const { email } = req.body;
+    const user = await User.findOne({email});
+
+    if(!user) {
+      throw HttpError(404, "User not found")
     }
 
-    const user = await User.findOne({email});
-    if(!user) {
+    if(user.verify) {
+      throw HttpError(400, "Verification has already been passed")
+    }
+
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${user.verificationToken}">Verification email sent</p>`,
+    };
+
+    await sendEmail(verifyEmail);
+
+    res.json({
+      message: "Verification email sent"
+    })
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+
+const login = async (req, res, next) => {
+  try {
+    const { error } = registerShema.validate(req.body);
+    const { email, password } = req.body;
+    if (error) {
+      throw HttpError(400, "Error from Joi or other validation library");
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
       throw HttpError(401, "Email or password is wrong");
+    }
+
+    if(!user.verify) {
+      throw HttpError(401, "Email not verify");
     }
     const passwordCompare = await bcrypt.compare(password, user.password);
 
-    if(!passwordCompare) {
+    if (!passwordCompare) {
       throw HttpError(401, "Email or password is wrong");
     }
 
     const payload = {
-      id: user._id
-    }
+      id: user._id,
+    };
 
-    const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "24h"});
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
 
-    await User.findByIdAndUpdate(user._id, {token});
+    await User.findByIdAndUpdate(user._id, { token });
 
     res.json({
       token,
-      user
-    })
+      user,
+    });
   } catch (error) {
     next(error);
   }
-}
+};
 
-const getCurrent = async(req, res, next) => {
+const getCurrent = async (req, res, next) => {
   try {
-    const {error} = registerSchema.validate(req.body);
-    if(error) {
-      throw HttpError(400, "Error from Joi or other validation library")
+    const { error } = registerShema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Error from Joi or other validation library");
     }
-    const {email, subscription} = req.user;
+    const { email, subscription } = req.user;
 
     res.json({
       email,
-      subscription
-    })
+      subscription,
+    });
   } catch (error) {
     next(error);
   }
+};
 
-}
-
-const logout = async(req, res, next) => {
+const logout = async (req, res, next) => {
   try {
-    const {error} = registerSchema.validate(req.body);
-    if(error) {
-      throw HttpError(400, "Error from Joi or other validation library")
+    const { error } = registerShema.validate(req.body);
+    if (error) {
+      throw HttpError(400, "Error from Joi or other validation library");
     }
-    const {_id} = req.user;
+    const { _id } = req.user;
 
-    await User.findByIdAndUpdate(_id, {token: ""});
+    await User.findByIdAndUpdate(_id, { token: "" });
 
     res.json({
-      message: "Logout success"
-    })
+      message: "Logout success",
+    });
   } catch (error) {
     next(error);
   }
-}
+};
+
 const updateAvatar = async (req, res, next) => {
   try {
     const { _id } = req.user;
@@ -116,9 +194,7 @@ const updateAvatar = async (req, res, next) => {
     jimp
       .read(`./temp/${filename}`)
       .then((img) => {
-        return img
-        .resize(250, 250)
-        .write(`./public/avatars/${avatarName}`);
+        return img.resize(250, 250).write(`./public/avatars/${avatarName}`);
       })
       .catch((err) => {
         console.log(err);
@@ -140,8 +216,10 @@ const updateAvatar = async (req, res, next) => {
 
 module.exports = {
   getCurrent,
-  register, 
+  register,
   login,
   logout,
   updateAvatar,
-}
+  verify,
+  resendVerify
+};
